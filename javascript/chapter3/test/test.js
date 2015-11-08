c98b1956077ba8b0de3f9c51181e8e447553f008
kongdehua <wenzhengko@sina.com>
2015-11-08 09:51:13 +0800

    检索对象

--- javascript/chapter3/test/test.js
+++ javascript/chapter3/test/test.js
@@ -1,1 +1,26 @@
 document.writeln('Hello world!');
+
+var empty_object = {};
+var stooge = {
+	"first-name": "Jerome",
+	"last-name": "Howard"
+};
+
+var flight = {
+	airline: "Oceanic",
+	number: 815,
+	departure: {
+		IATA: "SYD",
+		time: "2004-09-23",
+		city: "Sydney"
+	},
+	arrival: {
+		IATA: "LAX",
+		time: "2004-09-22",
+		city: "Los Angleles"
+	}
+};
+
+document.writeln(stooge["first-name"])
+document.writeln(flight.arrival.IATA)
+document.writeln(flight.arrival.IATAS)
